3aefb35ebeb7c9f334f17f995742fc00c06963bc
manyamOpenthrive <manyam.c@openthrive.com>
2025-10-27 12:38:07 +0530

    few updates

--- storefront/src/app/[countryCode]/(main)/not-found.tsx
+++ storefront/src/app/[countryCode]/(main)/not-found.tsx
@@ -12,7 +12,7 @@ export default function NotFound() {
     <div className="flex flex-col gap-4 items-center justify-center min-h-[calc(100vh-64px)]">
       <h1 className="text-2xl-semi text-ui-fg-base">Page not found</h1>
       <p className="text-small-regular text-ui-fg-base">
-        The page you tried to access does not exist.
+        The page you tried to access does not exist..
       </p>
       <InteractiveLink href="/">Go to frontpage</InteractiveLink>
     </div>
